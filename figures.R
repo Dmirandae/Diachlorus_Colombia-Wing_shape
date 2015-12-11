477419e7b22f086a452f79140ba6dfa0ad9c82f6
Ambrosio Torres Galvis <atorresgalvis@gmail.com>
2015-12-11 09:12:44 -0500

    Update figures.R

--- figures.R
+++ figures.R
@@ -1,5 +1,5 @@
 ## Ambrosio Torres & Daniel Rafael Miranda-Esquivel (Laboratorio de Sistemática y Biogeografía, Universidad Industrial de Santander, Bucaramanga, Colombia)
-## Research: Wing shape variation in the taxonomic recognition of species of Diachlorus Osten-Sacken (Diptera: Tabanidae) from Colombia 
+## Research: Geometric wing variation in the taxonomic recognition of species of the genus Diachlorus Osten-Sacken (Diptera: Tabanidae) from Colombia
 ## Part: intraspecific and interspecific variation in taxonomy of the species of genus Diachlorus Osten-Sacken (Diptera: Tabanidae) of Colombia
 ## R version 3.1.2 & Rstudio 0.97.551
 ## 23th January 2015 
@@ -25,7 +25,7 @@ fig2 <- fig2 + scale_shape_manual(values=c(18, 15, 16, 17, 8, 2))
 fig2 <- fig2 + theme(axis.text=element_text(size=10), axis.title=element_text(size=12)) + theme(legend.position="none") + scale_y_continuous(name="PC2 (11%)") 
 fig2 <- fig2 + scale_x_continuous(name="PC1 (45%)") + theme(panel.background = element_rect(fill = "grey"))
 tiff("Fig2.tiff", height = 95, width = 174, units = 'mm', type="cairo", res=1000, compression = "lzw")
-fig2
+fig2 + theme(text=element_text(family="Calibri"))
 dev.off()
 ######################################################################################################################################################################################
 
@@ -33,7 +33,7 @@ dev.off()
 ## FIGURE 3
 tiff("Fig3.tiff", height = 95, width = 174, units = 'mm', type="cairo", res=1000, compression = "lzw")
 par(mar=c(0.5, 4, 0, 0))
-par(oma= c(0,0,0,0))
+par(oma= c(0,0,0,0)); par(family="Calibri")
 op = par(bg = "gray")
 par(lwd=1.2)
 plot(clusDendro,horiz=F,axes=T, ylab= "Height", cex.axis=1,cex.lab=1)
@@ -43,14 +43,14 @@ dev.off()
 ######################################################################################################################################################################################
 ## FIGURE 4
 tiff("Fig4.tiff", height = 95, width = 174, units = 'mm', type="cairo", res=1000, compression = "lzw")
-multiplot((p_curvi + theme(panel.background = element_rect(fill = "grey")) + theme(axis.title.x = element_blank()) + theme(legend.position="none") + theme(axis.text=element_text(size=10), axis.title=element_text(size=12))),(p_fusci + theme(panel.background = element_rect(fill = "grey")) + theme(legend.position="none") + theme(axis.text=element_text(size=10), axis.title=element_text(size=12))), (p_jobi + theme(panel.background = element_rect(fill = "grey")) + theme(legend.position="none") + theme(axis.title.x = element_blank()) + theme(axis.title.y = element_blank()) + theme(axis.text=element_text(size=10))), (p_leuco + theme(panel.background = element_rect(fill = "grey")) + theme(legend.position="none") + theme(axis.title.y = element_blank()) + theme(axis.text=element_text(size=10))), cols=2)
+multiplot((p_curvi + theme(text=element_text(family="Calibri")) + theme(panel.background = element_rect(fill = "grey")) + theme(axis.title.x = element_blank()) + theme(legend.position="none") + theme(axis.text=element_text(size=10), axis.title=element_text(size=12))),(p_fusci + theme(text=element_text(family="Calibri")) + theme(panel.background = element_rect(fill = "grey")) + theme(legend.position="none") + theme(axis.text=element_text(size=10), axis.title=element_text(size=12))), (p_jobi + theme(text=element_text(family="Calibri")) + theme(panel.background = element_rect(fill = "grey")) + theme(legend.position="none") + theme(axis.title.x = element_blank()) + theme(axis.title.y = element_blank()) + theme(axis.text=element_text(size=10))), (p_leuco + theme(text=element_text(family="Calibri")) + theme(panel.background = element_rect(fill = "grey")) + theme(legend.position="none") + theme(axis.title.y = element_blank()) + theme(axis.text=element_text(size=10))), cols=2)
 dev.off()
 ######################################################################################################################################################################################
 
 ######################################################################################################################################################################################
 ## FIGURE 5
 tiff("Fig5.tiff", height = 75, width = 120, units = 'mm', type="cairo", res=1000, compression = "lzw")
-par(mar=c(0.8, 4.5, 0, 0))
+par(mar=c(0.8, 4.5, 0, 0)); par(family="Calibri")
 par(oma= c(0,0,0,0))
 op = par(bg = "gray")
 par(lwd=1.5)
